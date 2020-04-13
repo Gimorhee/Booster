f592a756387d4360ebb0c47e4c6511e0550b38c2
Danny Rhee <dongyunrhee@gmail.com>
2020-04-12 22:45:10 -0700

    Responsiveness on Login / SubNavBar Components

--- src/components/NavBar/SubNavBar.js
+++ src/components/NavBar/SubNavBar.js
@@ -8,22 +8,28 @@ export const SubNavBar = () => {
     <div className="SubNavBar">
       <div className="Inner-Container">
         <Link className="SubNav-Link" to="/">
-          Overview
+          <i class="fab fa-bootstrap SubNav-Logo"></i>
+          <span>Overview</span>
         </Link>
         <Link className="SubNav-Link" to="/features">
-          Features
+          <i className="fas fa-atom SubNav-Logo"></i>
+          <span>Features</span>
         </Link>
         <Link className="SubNav-Link" to="/howtouse">
-          How To Use
+          <i className="fas fa-question-circle SubNav-Logo"></i>
+          <span>Directions</span>
         </Link>
         <Link className="SubNav-Link" to="/games">
-          Supported Games
+          <i className="fas fa-dice-d6 SubNav-Logo"></i>
+          <span>Games</span>
         </Link>
         <Link className="SubNav-Link" to="/pricing">
-          Pricing
+          <i className="fas fa-hand-holding-usd SubNav-Logo"></i>
+          <span>Pricing</span>
         </Link>
         <Link className="SubNav-Link" to="/extensions">
-          Extensions
+          <i className="fas fa-puzzle-piece SubNav-Logo"></i>
+          <span>Extensions</span>
         </Link>
       </div>
     </div>
